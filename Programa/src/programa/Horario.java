f797ce32f880482fbb1b588191e2fdf8989d2f98
SrDeLorean <xebaelvemgador@gmail.com>
2020-04-04 18:16:11 -0300

    Clase horario funcionando
    La clase horario se encuentra funcionando dado que paso todas las pruebas, cabe destacar que me equivoque al crear un if y el tdd me salvo y lo arregle, le puse && y era || al ultimo iff

--- Programa/src/programa/Horario.java
+++ Programa/src/programa/Horario.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package programa;
 
 /**
@@ -15,11 +10,47 @@ public class Horario {
     private String termino;
 
     public boolean crearHorario(String inicio, String termino) {
-        return false;
+        String[] ini = inicio.split(":");
+        String[] ter = termino.split(":");
+        if(!ini[1].equals("00") || !ter[1].equals("00")){
+            System.out.println("cai aca 1");
+            return false;
+        }
+        int numIni = Integer.parseInt(ini[0]);
+        int numTer = Integer.parseInt(ter[0]);
+        if(numTer-numIni!=1){
+            System.out.println("cai aca 2");
+            return false;
+        }
+        if(numIni<8 || numIni>22){
+            System.out.println("cai aca 3");
+            return false;
+        }
+        this.inicio=inicio;
+        this.termino=termino;
+        return true;
     }
     
     public boolean modificarHorario(String inicio, String termino){
-        return false;
+        String[] ini = inicio.split(":");
+        String[] ter = termino.split(":");
+        if(!ini[1].equals("00") || !ter[1].equals("00")){
+            System.out.println("cai aca 1");
+            return false;
+        }
+        int numIni = Integer.parseInt(ini[0]);
+        int numTer = Integer.parseInt(ter[0]);
+        if(numTer-numIni!=1){
+            System.out.println("cai aca 2");
+            return false;
+        }
+        if(numIni<8 || numIni>22){
+            System.out.println("cai aca 3");
+            return false;
+        }
+        this.inicio=inicio;
+        this.termino=termino;
+        return true;
     }
 
     public String getInicio() {
