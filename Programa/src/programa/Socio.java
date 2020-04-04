ba3936b03e69c60a9b55e402e02f4da1efb1f1ad
SrDeLorean <xebaelvemgador@gmail.com>
2020-04-04 19:16:12 -0400

    Clace Socio funcionando
    Las validaciones se encuentran correctas y el tdd funciona

--- Programa/src/programa/Socio.java
+++ Programa/src/programa/Socio.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package programa;
 
 /**
@@ -14,14 +9,133 @@ public class Socio {
     private String rut;
     private String nombre;
     
+    /**
+     * Metodo encargador de crear un socio
+     * Los parametros que se requieren son el rut y el nombre, 2 string
+     * @param rut
+     * @param nombre
+     * @return 
+     */
     public boolean crearSocio(String rut, String nombre){
-        
-        return false;
+        if(!(rut instanceof String)){
+            return false;
+        }
+        if(!(nombre instanceof String)){
+            return false;
+        }
+        if(nombre.equals("")){
+            return false;
+        }
+
+        if(rut.split("\\.").length!=3){
+            return false;
+        }
+        if(!"".equals(rut)){
+            String[] guion = rut.split("-");
+            if(guion.length == 2){
+                char[] digVerificador = guion[1].toCharArray();
+                if(digVerificador.length == 1){
+                    if(!(( digVerificador[0] >=48 && digVerificador[0]<=57)  || digVerificador[0]==   107 || digVerificador[0] == 75 )){
+                        return false;
+                    }                                         
+                }
+                else{
+                    return false;
+                }                
+                if( (guion[0].charAt(guion[0].length()-4 ) == '.')  && (guion[0].charAt(guion[0].length()-8 ) == '.') ){
+                    String[] puntos = guion[0].split("\\.");
+                    char[] puntos1 = puntos[0].toCharArray();
+                    char[] puntos2 = puntos[1].toCharArray();
+                    char[] puntos3 = puntos[2].toCharArray();
+                    if(puntos.length == 3 && puntos2.length == 3 && puntos3.length ==3 && puntos1.length >=1 && puntos1.length <=3){                                  
+                        char[] numeros = guion[0].replace(".","").toCharArray();                           
+                        if(numeros.length >=7 && numeros.length <=9 && !guion[0].startsWith("0") && !guion[0].startsWith("00") && !guion[0].startsWith("000")){
+                            for(char c:numeros){                                
+                                if( !(c>=48 && c<=57) ){
+                                    return false;
+                                }
+                            }
+                        }else{
+                            return false;
+                        }
+                    }else{
+                        return false;
+                    } 
+                }   else{
+                    return false;
+                }
+            }else{
+                return false;
+            }                
+        }else{
+            return false;
+        }
+        this.nombre=nombre;
+        this.rut=rut;
+        return true;
     }
     
+    /**
+     * Metodo encargado de modificar los campos de un socio
+     * Los parametros que se requieren son el rut y el nombre, 2 string
+     * @param rut
+     * @param nombre
+     * @return 
+     */
     public boolean modificarSocio(String rut, String nombre){
-        
-        return false;
+        if(!(rut instanceof String)){
+            return false;
+        }
+        if(!(nombre instanceof String)){
+            return false;
+        }
+        if(nombre.equals("")){
+            return false;
+        }
+        //Metodo sacado del codigo de EventTinder :)
+        if(!"".equals(rut)){
+            String[] guion = rut.split("-");
+            if(guion.length == 2){
+                char[] digVerificador = guion[1].toCharArray();
+                if(digVerificador.length == 1){
+                    if(!(( digVerificador[0] >=48 && digVerificador[0]<=57)  || digVerificador[0]==   107 || digVerificador[0] == 75 )){
+                        return false;
+                    }                                         
+                }
+                else{
+                    return false;
+                }                
+                if( (guion[0].charAt(guion[0].length()-4 ) == '.')  && (guion[0].charAt(guion[0].length()-8 ) == '.') ){
+                    String[] puntos = guion[0].split("\\.");
+                    char[] puntos1 = puntos[0].toCharArray();
+                    char[] puntos2 = puntos[1].toCharArray();
+                    char[] puntos3 = puntos[2].toCharArray();
+                    if(puntos.length == 3 && puntos2.length == 3 && puntos3.length ==3 && puntos1.length >=1 && puntos1.length <=3){                                  
+                        char[] numeros = guion[0].replace(".","").toCharArray();                           
+                        if(numeros.length >=7 && numeros.length <=9 && !guion[0].startsWith("0") && !guion[0].startsWith("00") && !guion[0].startsWith("000")){
+                            for(char c:numeros){                                
+                                if( !(c>=48 && c<=57) ){
+                                    return false;
+                                }
+                            }
+                        }else{
+                            return false;
+                        }
+                    }else{
+                        return false;
+                    } 
+                }   else{
+                    return false;
+                }
+            }else{
+                return false;
+            }                
+        }else{
+            return false;
+        }
+        this.nombre=nombre;
+        this.rut=rut;
+        return true;
     }
 
     public String getRut() {
