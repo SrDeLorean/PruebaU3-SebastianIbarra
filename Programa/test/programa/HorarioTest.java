817c39bdc1aef53b3e75a2dbc6689715e0eb2613
SrDeLorean <xebaelvemgador@gmail.com>
2020-04-04 17:59:40 -0300

    TDD de la clase Horario
    Se priorizaron las pruebas en crearHorario

--- Programa/test/programa/HorarioTest.java
+++ Programa/test/programa/HorarioTest.java
@@ -43,14 +43,30 @@ public class HorarioTest {
     @Test
     public void testCrearHorario() {
         System.out.println("crearHorario");
-        String inicio = "";
-        String termino = "";
+        
         Horario instance = new Horario();
-        boolean expResult = false;
-        boolean result = instance.crearHorario(inicio, termino);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        boolean result = instance.crearHorario("12:00", "13:00");
+        assertEquals(true, result);
+        
+        instance = new Horario();
+        result = instance.crearHorario("12:00", "13:30");
+        assertEquals(false, result);
+        
+        instance = new Horario();
+        result = instance.crearHorario("12:00", "14:00");
+        assertEquals(false, result);
+        
+        instance = new Horario();
+        result = instance.crearHorario("23:00", "24:00");
+        assertEquals(false, result);
+        
+        instance = new Horario();
+        result = instance.crearHorario("12:30", "13:30");
+        assertEquals(false, result);
+        
+        instance = new Horario();
+        result = instance.crearHorario("00:00", "00:00");
+        assertEquals(false, result);
     }
 
     /**
@@ -59,42 +75,12 @@ public class HorarioTest {
     @Test
     public void testModificarHorario() {
         System.out.println("modificarHorario");
-        String inicio = "";
-        String termino = "";
         Horario instance = new Horario();
-        boolean expResult = false;
-        boolean result = instance.modificarHorario(inicio, termino);
+        boolean expResult = true;
+        boolean result = instance.modificarHorario("12:00", "13:00");
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of getInicio method, of class Horario.
-     */
-    @Test
-    public void testGetInicio() {
-        System.out.println("getInicio");
-        Horario instance = new Horario();
-        String expResult = "";
-        String result = instance.getInicio();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of getTermino method, of class Horario.
-     */
-    @Test
-    public void testGetTermino() {
-        System.out.println("getTermino");
-        Horario instance = new Horario();
-        String expResult = "";
-        String result = instance.getTermino();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        assertEquals(instance.getInicio(), "12:00");
+        assertEquals(instance.getTermino(), "13:00");
     }
     
 }
