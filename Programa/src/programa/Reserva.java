220695a162b4b6a6e0b6b2706c93bed96d617953
SrDeLorean <xebaelvemgador@gmail.com>
2020-04-04 19:54:32 -0300

    Clase Reserva completada
    pasa todo el tdd, recuerdo que tuve que edicar editar el tdd en la parte de horario=null por el instanceof

--- Programa/src/programa/Reserva.java
+++ Programa/src/programa/Reserva.java
@@ -17,13 +17,41 @@ public class Reserva {
     private int refSocio;
 
     public boolean crearReserva(Horario horario, String dia, int idCancha, int refSocio) {
-        
-        return false;
+        if(!(horario instanceof Horario)){
+            return false;
+        }
+        if(!(dia instanceof String)){
+            return false;
+        }
+        if(!(Integer.class.isInstance(idCancha) && Integer.class.isInstance(refSocio))){
+            return false;
+        }
+        if((idCancha<1 || idCancha>4) || refSocio<0){
+            return false;
+        }
+        if(!dia.equals("lunes") && !dia.equals("martes") && !dia.equals("miercoles") && !dia.equals("jueves") && !dia.equals("viernes") && !dia.equals("sabado") &&!dia.equals("domingo")){            
+            return false;
+        }
+        this.horario=horario;
+        this.dia=dia;
+        this.idCancha=idCancha;
+        this.refSocio=refSocio;
+        return true;
     }
     
     public boolean modificarReserva(Horario horario, String dia) {
-        
-        return false;
+        if(!(horario instanceof Horario)){
+            return false;
+        }
+        if(!(dia instanceof String)){
+            return false;
+        }
+        if(!dia.equals("lunes") && !dia.equals("martes") && !dia.equals("miercoles") && !dia.equals("jueves") && !dia.equals("viernes") && !dia.equals("sabado") &&!dia.equals("domingo")){
+            return false;
+        }
+        this.horario=horario;
+        this.dia=dia;
+        return true;
     }
 
     public Horario getHorario() {
